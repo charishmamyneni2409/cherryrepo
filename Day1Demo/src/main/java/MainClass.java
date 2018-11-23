48ad66bd61d0ec69a6de074e3c3bb9fa5fe56e43
mycharis <mycharis@DIN73002722.corp.capgemini.com>
2018-11-23 14:25:34 +0530

    greet method added

--- Day1Demo/src/main/java/MainClass.java
+++ Day1Demo/src/main/java/MainClass.java
@@ -4,11 +4,16 @@ public class MainClass {
 	public static void main(String[] args) {
 	System.out.println( "Hello !Whats up?");
 	greetings();
-
+greet();
 	}
 	public static void greetings()
 	{
 		System.out.println("Welcome!");
 	}
+	public static void greet()
+	{
+		System.out.println("Welcome Sony!");
+	}
+
 
 }
