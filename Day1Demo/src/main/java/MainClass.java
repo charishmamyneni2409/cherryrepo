d30581dd3712707bd0725b9fd09fd7623305b81a
ashukl10 <ashukl10@DIN73002715.corp.capgemini.com>
2018-11-23 15:42:44 +0530

    Aman Commit

--- Day1Demo/src/main/java/MainClass.java
+++ Day1Demo/src/main/java/MainClass.java
@@ -1,23 +1,24 @@
-
 public class MainClass {
 
 	public static void main(String[] args) {
-	System.out.println( "Hello !Whats up?");
-	greetings();
-greet();
+		System.out.println("Hello !Whats up?");
+		greetings();
+		greet();
 	}
-	public static void greetings()
-	{
+
+	public static void greetings() {
 		System.out.println("Welcome!");
 	}
-	public static void greet()
-	{
+
+	public static void greet() {
 		System.out.println("Welcome Sony!");
 	}
 
-	public static void show()
-	{
+	public static void show() {
 		System.out.println("Welcome user!");
 	}
 
+	public static void hello() {
+		System.out.println("Hello World!!");
+	}
 }
